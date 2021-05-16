9c575113d008a90215c0144514bd19b8bb0cd8bd
raju.kapadne09@gmail.com <Shimpi@1989>
2021-05-16 15:23:33 +0530

    First commit uploading all the files.

---
+++ it-online-exam/src/main/java/com/kapadne/it/it/online/exam/ItOnlineExamApplication.java
@@ -0,0 +1,13 @@
+package com.kapadne.it.it.online.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItOnlineExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItOnlineExamApplication.class, args);
+	}
+
+}
---
+++ it-online-exam/src/test/java/com/kapadne/it/it/online/exam/ItOnlineExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapadne.it.it.online.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItOnlineExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
